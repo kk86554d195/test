c6cd13a6b23ff8551680d3dc9eca56059ceeda5e
kk86554d195 <43671191+kk86554d195@users.noreply.github.com>
2024-08-16 12:05:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,3 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-const dataFile = 'data.txt';
-
-// 處理從前端發送來的資料並記錄到 data.txt
-app.post('/api/send-data', (req, res) => {
-  const receivedData = req.body.data;
-  
-  // 獲取當前時間
-  const currentTime = new Date().toLocaleString(); 
-  
-  // 將資料與當前時間結合
-  const modifiedData = `${receivedData} ${currentTime}`;
-  
-  // 將資料記錄到檔案中
-  fs.appendFile(dataFile, modifiedData + '\n', (err) => {
-    if (err) {
-      console.error('無法寫入檔案:', err);
-      res.status(500).send('伺服器錯誤');
-      return;
-    }
-    
-    console.log('資料已記錄:', modifiedData);
-    res.status(200).send({ modifiedData: modifiedData });
-  });
-});
-
-// 從 data.txt 讀取資料並返回給前端
 app.get('/api/get-data', (req, res) => {
   fs.readFile(dataFile, 'utf8', (err, data) => {
     if (err) {
@@ -54,11 +17,7 @@ app.get('/api/get-data', (req, res) => {
       }
     }).filter(item => item !== null);
     
-    // 返回解析後的 JSON 資料
-    res.status(200).send({ data: jsonData });
+    // 直接返回解析後的 JSON 資料列表
+    res.status(200).json(jsonData);
   });
-});
-
-app.listen(3000, () => {
-  console.log('伺服器正在3000端口運行');
 });
